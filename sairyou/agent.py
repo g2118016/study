070e88fd6a46f2bfc81a904751bcc8259c12ed11
g2118016 <b1014157@gmail.com>
2018-07-22 03:45:31 +0900

    iroiro

--- sairyou/agent.py
+++ sairyou/agent.py
@@ -6,18 +6,27 @@ import copy
 class Agent:
 
     # constructor
-    def __init__(self, maze_shape):
-        self.state = numpy.array([0, 0])
+    def __init__(self, maze_shape, start):
+        #初期位置座標
+        self.state = numpy.array(start)
+        #各マスにおける可能な行動を生成 十字キーに0~3を振り分ける
         self.actions = self.__create_actions(maze_shape)
+        #十字キーと実際の動きを対応させる
         self.moves = {0: numpy.array([0, -1]), 1: numpy.array([1, 0]), 2: numpy.array([0, 1]), 3: numpy.array([-1, 0])}
+        #4*ステージの大きさのQ値格納用のリスト？
+        #各マスの各行動（上，下，左，右）の行動ごとの価値を入れる
         self.q = numpy.zeros((4, ) + maze_shape)
 
 
 # public method
     def act(self, maze, epsilon, alpha, gamma):
+        #行動を選択（0~3）
         act_index = self.__select_action(epsilon)
+        #行動に対応する座標を取得
         move = self.moves.get(act_index)
+        #移動した先の報酬を取得
         reward = maze[tuple(self.state + move)]
+        #Q値の更新
         self.update_q(act_index, move, reward, alpha, gamma)
         self.state += move
 
@@ -28,11 +37,33 @@ class Agent:
         self.q[act_index, y, x] = _q + alpha * (reward + gamma * self.__get_max_q(move) - _q)
 
 
+    """
     def goal(self, maze_shape):
+        #右下についたらゴールと判定？
         return numpy.array_equal(self.state, numpy.array(maze_shape) - 1)
+    """
 
+    #変更　2018/7/22
+    def goal(self, reward_indexes, counter, limit, poke_location, maze):
+        #現在地がポケストップか判定
+        for i in range(len(reward_indexes)):
+            if numpy.allclose(self.state, reward_indexes[i]):
+                counter+=1
+                #後で使うのでポケストップの場所を保存
+                copy_index=copy.deepcopy(reward_indexes[i]).tolist()
+                poke_location.append(copy_index)
+                #1回通ったポケストップをカウントしないように削除
+                numpy.delete(reward_indexes, copy_index)
+                #通ったポケストップの報酬をただの道と同じ0にする
+                maze[tuple(copy_index)]=0
+                break
+        if counter==limit:
+            return True
+        else:
+            return False
 
     def reset(self):
+        #初期位置へ戻す
         self.state = numpy.array([0, 0])
 
 
@@ -62,12 +93,16 @@ class Agent:
 
 
     def __select_action(self, epsilon):
+        #現在の位置を取得
         y, x = self.state
+        #現在のマスからとれる行動（の選択肢）を取得
         action = copy.deepcopy(self.actions[y, x])
+        #0~1未満の数字を生成，εより大きければQ値によって行動を選択
         if numpy.random.rand() > epsilon:
             mode = '!!!greedy!!!'
             act_index = self.__select_greedy_action(action)
         else:
+            #小さければランダムな行動をとる
             mode = '!!!random!!!'
             act_index = self.__select_random_action(action)
 
@@ -78,8 +113,11 @@ class Agent:
 
     def __select_greedy_action(self, action):
         y, x = self.state
+        #価値が最大になる行動の価値（Q値）を取ってくる
         _max = self.q[action, y, x].max()
+        #そのインデックスを取得
         _indexes = list(numpy.argwhere(self.q[action, y, x] == _max))
+        #価値が最大の行動が複数ある場合はその中からランダムに行動を選ぶ
         random.shuffle(_indexes)
         #[0]を追加．_indexes[0]が[数字]になってたので
         return action[_indexes[0][0]]
@@ -91,6 +129,9 @@ class Agent:
 
 
     def __get_max_q(self, move):
+        #移動先のマスの座標
         y, x = self.state + move
+        #移動先で取れる行動の選択肢
         move = self.actions[y, x]
+        #移動先のQ値の最大値を返す
         return self.q[move, y, x].max()
--- sairyou/main.py
+++ sairyou/main.py
@@ -4,12 +4,38 @@ from mazeimage import *
 if __name__ == '__main__':
 
     # init
+    #ランダム行動確率ε
     epsilon = 0.1
+    #学習率
     alpha = 0.2
+    #割引率
     gamma = 0.9
+    #csvファイル読み込み
     maze = numpy.loadtxt('./resources/maze.csv', delimiter = ',')
-    agent = Agent(maze.shape)
-    maze_image = MazeImage(maze, 300, 300)
+
+
+    #追加　2018/7/22
+    #初期一座標を手動で入力
+    x=input("start_x:"+"0~"+str(maze.shape[1]-1)+">>")
+    y=input("start_y:"+"0~"+str(maze.shape[0]-1)+">>")
+    start_x=int(x)
+    start_y=int(y)
+    start=[start_y, start_x]
+
+    #ますの中で最大の報酬を取得
+    max_reward=maze.max()
+    #その座標を取得
+    reward_indexes = numpy.argwhere(maze == max_reward)
+    #ポケストップを通った数を数えるカウンター
+    counter=0
+    #終了条件：n回ポケストップを通ったらゴール
+    limit=10
+    #通ったポケストップの座標を記録
+    poke_location=[start]
+
+
+    agent = Agent(maze.shape, start)
+    maze_image = MazeImage(maze, 600, 600)
 
     trial = 0
     while True:
@@ -20,8 +46,12 @@ if __name__ == '__main__':
         agent.act(maze, epsilon, alpha, gamma)
         maze_image.save_movie()
 
-        if agent.goal(maze.shape):
-            print('\033[32m' + '!!!goal!!!' + '\033[0m')
+        #if agent.goal(maze.shape):
+
+        #変更
+        if agent.goal(reward_indexes, counter, limit, poke_location, maze):
+            #\033[32mとか\033[0mは文字色をつけたりするコンソール制御っぽい？
+            print('' + '!!!goal!!!' + '')
             trial += 1
             print('next trial: %d' % trial)
             agent.reset()
@@ -31,4 +61,7 @@ if __name__ == '__main__':
 
     maze_image.save_movie()
     cv2.imwrite('shortest.png', maze_image.shortest_path(agent.q))
+
+    #初期座標と通ったポケストップの座標表示
+    print(poke_location)
     cv2.destroyAllWindows()
